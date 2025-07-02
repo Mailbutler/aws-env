7c8f599d5d490d155a7b27f2216e0dbf1f8e7277
Fabian Jäger <fabian@mailbutler.io>
2025-07-02 13:48:18 +0200

    Support multiple environments to load environment variables from

--- aws-env.go
+++ aws-env.go
@@ -3,12 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ssm"
 	"log"
 	"os"
 	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ssm"
 )
 
 const (
@@ -17,6 +18,7 @@ const (
 )
 
 func main() {
+	// Check if AWS_ENV_PATH is set
 	if os.Getenv("AWS_ENV_PATH") == "" {
 		log.Println("aws-env running locally, without AWS_ENV_PATH")
 		return
@@ -26,15 +28,18 @@ func main() {
 	format := flag.String("format", formatExports, "output format")
 	flag.Parse()
 
-	if *format == formatExports || *format == formatDotenv {
-	} else {
+	if *format != formatExports && *format != formatDotenv {
 		log.Fatal("Unsupported format option. Must be 'exports' or 'dotenv'")
 	}
 
 	sess := CreateSession()
 	client := CreateClient(sess)
 
-	ExportVariables(client, os.Getenv("AWS_ENV_PATH"), *recursivePtr, *format, "")
+	// if AWS_ENV_PATH contains a comma-separated list, export variables for each path
+	paths := strings.Split(os.Getenv("AWS_ENV_PATH"), ",")
+	for _, path := range paths {
+		ExportVariables(client, strings.TrimSpace(path), *recursivePtr, *format, "")
+	}
 }
 
 func CreateSession() *session.Session {
