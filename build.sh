9bedc1a753e4d3383f4cba67274c391755917eb2
Fabian Jäger <fabian@mailbutler.io>
2025-07-02 13:54:39 +0200

    Initialize Go module and add dependencies

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 BUILD_DIR=bin
 NAME=aws-env
 
-mkdir $BUILD_DIR
+mkdir -p $BUILD_DIR
 
 for GOOS in darwin linux windows; do
-    for GOARCH in 386 amd64; do
+    for GOARCH in amd64 arm64; do
         GOOS=$GOOS GOARCH=$GOARCH go build -v -o $BUILD_DIR/$NAME-$GOOS-$GOARCH
     done
 done
